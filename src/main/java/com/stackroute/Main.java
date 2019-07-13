8b5e95fda72ec51ea8d3c1b234935abce999aaaa
Sabya7 <sabya62.ss@gmail.com>
2019-07-13 16:36:55 +0530

    task1

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,19 @@
+package com.stackroute;
+
+import com.stackroute.domain.Actor;
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
+        context.register(Config.class);
+        context.refresh();
+        Movie movie=context.getBean("movie", Movie.class);
+        //Actor actor=context.getBean("actor", Actor.class);
+        System.out.println(movie);
+    }
+}
