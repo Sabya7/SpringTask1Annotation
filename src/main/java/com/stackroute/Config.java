1b65e7d7d2176ca051652a4e022f74c83fee07b7
Sabya7 <sabya62.ss@gmail.com>
2019-07-13 16:53:33 +0530

    task2

--- src/main/java/com/stackroute/Config.java
+++ src/main/java/com/stackroute/Config.java
@@ -4,6 +4,7 @@ import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Config {
@@ -17,6 +18,25 @@ public class Config {
         return actor;
     }
     @Bean
+    public Actor actorMites()
+    {
+        Actor actor=new Actor("Mites","Male",21);
+        return actor;
+    }
+    @Bean
+    public Actor actorAbinash()
+    {
+        Actor actor=new Actor("Abinash","Male",21);
+        return actor;
+    }
+    @Bean
+    public Actor actorPaku()
+    {
+        Actor actor=new Actor("Paku","Male",21);
+        return actor;
+    }
+    @Bean({"movieA","movieB"})
+    @Scope("prototype")
     public Movie movie()
     {
         Movie movie =new Movie(actor());
--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -12,8 +12,9 @@ public class Main {
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
         context.register(Config.class);
         context.refresh();
-        Movie movie=context.getBean("movie", Movie.class);
+        Movie movie=context.getBean("movieB", Movie.class);
+        Movie movie1=context.getBean("movieA", Movie.class);
         //Actor actor=context.getBean("actor", Actor.class);
-        System.out.println(movie);
+        System.out.println(movie1==movie);
     }
 }
